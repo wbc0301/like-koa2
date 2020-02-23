6fff956044fa11d47f30c4a297591051855a177a
wbc <417780894@qq.com>
2020-02-23 23:57:18 +0800

    格式化

--- like-koa2.js
+++ like-koa2.js
@@ -1,54 +1,52 @@
 const http = require('http')
 
-// 组合中间件
-function compose(middlewareList) {
-    return function (ctx) {
-        function dispatch(i) {
-            const fn = middlewareList[i]
-            try {
-                return Promise.resolve(
-                    fn(ctx, dispatch.bind(null, i + 1))  // promise
-                )
-            } catch (err) {
-                return Promise.reject(err)
-            }
-        }
-        return dispatch(0)
+function compose(middlewareList) { // 组合中间件
+  return function (ctx) {
+    function dispatch(i) {
+      const fn = middlewareList[i]
+      try {
+        return Promise.resolve(
+          fn(ctx, dispatch.bind(null, i + 1))  // promise
+        )
+      } catch (err) {
+        return Promise.reject(err)
+      }
     }
+    return dispatch(0)
+  }
 }
 
 class LikeKoa2 {
-    constructor() {
-        this.middlewareList = []  
-    }
+  constructor() {
+    this.middlewareList = []
+  }
 
-    use(fn) {  // 接收中间件
-        this.middlewareList.push(fn)
-        return this
-    }
+  use(fn) {  // 接收中间件
+    this.middlewareList.push(fn)
+    return this
+  }
 
-    createContext(req, res) { // 创建 ctx 对象
-        const ctx = {
-            req,
-            res
-        }
-        ctx.query = req.query
-        return ctx
+  createContext(req, res) { // 创建 ctx 对象
+    const ctx = {
+      req,
+      res
     }
+    ctx.query = req.query
+    return ctx
+  }
 
-    callback() {
-        const fn = compose(this.middlewareList)
-
-        return (req, res) => {
-            const ctx = this.createContext(req, res)
-            return fn(ctx);
-        }
+  callback() {
+    const fn = compose(this.middlewareList)
+    return (req, res) => {
+      const ctx = this.createContext(req, res)
+      return fn(ctx);
     }
+  }
 
-    listen(...args) {
-        const server = http.createServer(this.callback())
-        server.listen(...args)
-    }
+  listen(...args) {
+    const server = http.createServer(this.callback())
+    server.listen(...args)
+  }
 }
 
 module.exports = LikeKoa2
